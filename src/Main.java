4e7c557f36abc7458c3857742eaa64e8b32db926
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2026-03-02 16:47:30 -0300

    Adiciona classe Main com exemplo de filme Top Gun Maverick

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,19 @@ public class Main{
         System.out.println("Esse é o screen Match");
         System.out.println("Filme: Top Gun: Maverick");
 
-        int ano = 2026;
+        int yearOfRelease = 2026;
+        System.out.println("Year Of Release: " + yearOfRelease);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.8) /3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = "após mais de 30 anos de serviço, Pete \"Maverick\" Mitchell (Tom Cruise) continua na ativa como um corajoso piloto de testes, evitando promoções para não deixar de voar. Ele é chamado para treinar uma nova equipe de graduados Top Gun para uma missão suicida e especial, confrontando os fantasmas de seu passado ao treinar Bradley Bradshaw (Rooster), filho de seu falecido amigo Goose";
+        System.out.println("Sinopse:" + sinopse);
+
     }
 }
+
+
