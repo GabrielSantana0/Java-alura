581b3d9bb4cbe1a75ce58d61cee99c9379bc1089
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2026-03-02 14:30:35 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Esse é o screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2026;
+    }
+}
