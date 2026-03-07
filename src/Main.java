4bd2bd6664c7855088e5275de6c52f2599f32fe5
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2026-03-07 04:09:13 -0300

    Avanço nos estudos

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main{
         System.out.println("Esse é o screen Match");
         System.out.println("Filme: Top Gun: Maverick");
 
-        int yearOfRelease = 2026;
+        int yearOfRelease = 2022;
         System.out.println("Year Of Release: " + yearOfRelease);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
@@ -12,10 +12,10 @@ public class Main{
         System.out.println(media);
 
         String sinopse;
-        sinopse = "após mais de 30 anos de serviço, Pete \"Maverick\" Mitchell (Tom Cruise) continua na ativa como um corajoso piloto de testes, evitando promoções para não deixar de voar. Ele é chamado para treinar uma nova equipe de graduados Top Gun para uma missão suicida e especial, confrontando os fantasmas de seu passado ao treinar Bradley Bradshaw (Rooster), filho de seu falecido amigo Goose";
+        sinopse = "após mais de 30 anos de serviço, Pete Mitchell (Tom Cruise) continua na ativa como um corajoso piloto de testes, evitando promoções para não deixar de voar. Ele é chamado para treinar uma nova equipe de graduados Top Gun para uma missão suicida e especial, confrontando os fantasmas de seu passado ao treinar Bradley Bradshaw (Rooster), filho de seu falecido amigo Goose";
         System.out.println("Sinopse:" + sinopse);
 
+        int classificacao = (int)  (media /2);
+        System.out.println("classificação:" + classificacao);
     }
 }
-
-
